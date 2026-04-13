76aaff80c9fb8b652d49683ee58cbea84af24e7d
M. Kaspar <archomath@gmail.com>
2026-04-13 23:11:54 +1000

    add cover image for player

--- src/components/player/Player.svelte
+++ src/components/player/Player.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+	import coverImage from "./cover-image.jpg";
+
 	let {
 		src,
 		title,
 	} = $props();
-
-	// const m3u = "https://vod-beta.stream.place/xrpc/place.stream.playback.getVideoPlaylist?uri=at://did:plc:rbvrr34edl5ddpuwcubjiost/place.stream.video/3mi2ikg6gij26";
 </script>
 
 <media-player
@@ -13,18 +13,12 @@
 >
 	<media-provider>
 		<source src={src} type="application/x-mpegurl">
-		<!-- <media-poster
+		<media-poster
 			class="vds-poster"
-			src="https://files.vidstack.io/sprite-fight/poster.webp"
-			alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
-		></media-poster> -->
+			src={coverImage}
+		></media-poster>
 	</media-provider>
-	<!-- <media-video-layout
-		// thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt"
-	></media-video-layout> -->
-	<media-plyr-layout
-		// thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt"
-	></media-plyr-layout>
+	<media-plyr-layout></media-plyr-layout>
 </media-player>
 
 <style>
