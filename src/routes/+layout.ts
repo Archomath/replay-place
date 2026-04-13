ff203f5f80e9ca873d6d3a22ec238be4bf1d6a54
M. Kaspar <archomath@gmail.com>
2026-04-13 22:48:04 +1000

    add extra vidstack style imports

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
 import "vidstack/bundle";
+import "vidstack/player/styles/base.css";
+import "vidstack/player/styles/plyr/theme.css";
+import "vidstack/player";
+import "vidstack/player/layouts/plyr";
+import "vidstack/player/ui";
 import HLS from "hls.js";
